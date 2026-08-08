a3c1d781c54d849b60e987906de5463e7a7e27c1
Hursh <hursh.nashine1998@gmail.com>
2026-08-08 11:12:30 -0400

    Add TEXT assets batch 1 of 4 (49.88 MiB)

---
+++ Assets/FirstGearGames/FlexSceneManager/Scripts/Core/Events/UnloadSceneEventArgs.cs
@@ -0,0 +1,41 @@
+﻿using FirstGearGames.FlexSceneManager.LoadUnloadDatas;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+namespace FirstGearGames.FlexSceneManager.Events
+{
+
+
+    public struct UnloadSceneStartEventArgs
+    {
+        /// <summary>
+        /// RawData used by the current scene action.
+        /// </summary>
+        public readonly UnloadSceneQueueData RawData;
+
+        public UnloadSceneStartEventArgs(UnloadSceneQueueData sqd)
+        {
+            RawData = sqd;
+        }
+    }
+
+    public struct UnloadSceneEndEventArgs
+    {
+        /// <summary>
+        /// RawData used by the current scene action.
+        /// </summary>
+        public readonly UnloadSceneQueueData RawData;
+        /// <summary>
+        /// Handles of scenes which were successfully unloaded.
+        /// </summary>
+        public int[] UnloadedSceneHandles;
+
+        public UnloadSceneEndEventArgs(UnloadSceneQueueData sqd, int[] unloadedHandles)
+        {
+            RawData = sqd;
+            UnloadedSceneHandles = unloadedHandles;
+        }
+    }
+
+
+}
---
+++ Assets/FirstGearGames/FlexSceneManager/Scripts/Core/Messages/SceneMessages.cs
@@ -0,0 +1,38 @@
+﻿using FirstGearGames.FlexSceneManager.LoadUnloadDatas;
+using Mirror;
+
+namespace FirstGearGames.FlexSceneManager.Messages
+{
+
+    /// <summary>
+    /// Sent to clients to load networked scenes.
+    /// </summary>
+    public struct LoadScenesMessage : NetworkMessage
+    {
+        public LoadSceneQueueData SceneQueueData;
+    }
+
+
+    /// <summary>
+    /// Sent to clients to unload networked scenes.
+    /// </summary>
+    public struct UnloadScenesMessage : NetworkMessage
+    {
+        public UnloadSceneQueueData SceneQueueData;
+    }
+
+
+    /// <summary>
+    /// Sent to server to indicate which scenes a client has loaded.
+    /// </summary>
+    public struct ClientScenesLoadedMessage : NetworkMessage
+    {
+        public SceneReferenceData[] SceneDatas;
+    }
+
+    public struct ClientPlayerCreated : NetworkMessage
+    {
+
+    }
+
+}
